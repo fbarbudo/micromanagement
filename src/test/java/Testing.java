8e6e49a87fdfea3e183e63a5d658a08c16057723
fbarbudo <paco.barbudo@gmail.com>
2016-04-10 19:25:00 +0200

    second

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.*;
@@ -13,6 +14,14 @@ public class Testing {
     public static final int WORKING_DAYS_IN_A_WEEK = 5;
     public static final int DAYS_IN_A_WEEKEND = 2;
 
+    private int workingHoursInADay;
+    private Team team;
+
+    @Before
+    public void setup() {
+        workingHoursInADay = 8;
+    }
+
     /**
      * TODO LIST
      *
@@ -25,40 +34,36 @@ public class Testing {
 
     @Test
     public void finaliza_un_proyecto_cuando_no_quedan_horas_por_ejecutar(){
-        Team team = new Team();
         LocalDate initDate = LocalDate.of(2016, Month.APRIL, 4);
 
-        assertEquals( LocalDate.of(2016, Month.APRIL, 19), calculateFinishDate(initDate, 81, team));
+        assertEquals( LocalDate.of(2016, Month.APRIL, 19), calculateFinishDate(initDate, 81, workingHoursInADay));
     }
 
     @Test
     public void finaliza_un_proyecto_cuando_no_queda_horas_por_ejecutar2(){
-        Team team = new Team();
         LocalDate initDate = LocalDate.of(2016, Month.APRIL, 4);
 
-        assertEquals( LocalDate.of(2016, Month.APRIL, 5), calculateFinishDate(initDate, 8, team));
+        assertEquals( LocalDate.of(2016, Month.APRIL, 5), calculateFinishDate(initDate, 8, workingHoursInADay));
     }
 
     @Test
     public void no_se_ejecutan_horas_el_dia_de_finalizacion(){
-        Team team = new Team();
         LocalDate initDate = LocalDate.of(2016, Month.APRIL, 6);
 
-        assertEquals( LocalDate.of(2016, Month.APRIL, 7), calculateFinishDate(initDate, 7, team));
+        assertEquals( LocalDate.of(2016, Month.APRIL, 7), calculateFinishDate(initDate, 7, workingHoursInADay));
     }
 
     @Test
     public void la_fecha_de_fin_no_puede_ser_en_fin_de_semana(){
-        Team team = new Team();
         LocalDate initDate = LocalDate.of(2016, Month.APRIL, 8);
 
-        assertEquals(LocalDate.of(2016, Month.APRIL, 11), calculateFinishDate(initDate, 8, team));
+        assertEquals(LocalDate.of(2016, Month.APRIL, 11), calculateFinishDate(initDate, 8, workingHoursInADay));
     }
 
 
-    private LocalDate calculateFinishDate(LocalDate initDate, int workingHours, Team team) {
+    private LocalDate calculateFinishDate(LocalDate initDate, int workingHours, int workingHoursInADay) {
 
-        int calendarDays = calculateCalendarDays(workingHours);
+        int calendarDays = calculateCalendarDays(workingHours, workingHoursInADay);
 
         LocalDate finishDate = initDate.plusDays(calendarDays);
 
@@ -69,17 +74,17 @@ public class Testing {
         return finishDate;
     }
 
-    private int calculateCalendarDays(int workingHours) {
-        int workingDays = calculateWorkingDays(workingHours);
+    private int calculateCalendarDays(int workingHours, int workingHoursInADay) {
+        int workingDays = calculateWorkingDays(workingHours, workingHoursInADay);
         int calendarDays = workingDays;
-        if ( calendarDays > WORKING_DAYS_IN_A_WEEK ){
+        if ( calendarDays > WORKING_DAYS_IN_A_WEEK){
             calendarDays += (calendarDays / WORKING_DAYS_IN_A_WEEK) * DAYS_IN_A_WEEKEND;
         }
         return calendarDays;
     }
 
-    private int calculateWorkingDays(int hours) {
-        return (hours / WORKING_HOURS_IN_A_DAY) + ((hours % WORKING_HOURS_IN_A_DAY) > 0? 1 : 0);
+    private int calculateWorkingDays(int hours, int workingHoursInADay) {
+        return (hours / workingHoursInADay) + ((hours % workingHoursInADay) > 0? 1 : 0);
     }
 
     private int daysToNextMonday(LocalDate date) {
@@ -91,9 +96,33 @@ public class Testing {
                 || date.getDayOfWeek().equals(DayOfWeek.SUNDAY);
     }
 
+
+    /**
+     * TODO_LIST
+     *
+     * el_trabajo_efectivo_de_un_equipo_es_un_porcentage_de_su_trabajo_diario
+     *
+     */
+
+    @Test
+    public void el_trabajo_efectivo_diario_de_un_equipo_es_un_porcentaje_de_su_trabajo_diario(){
+        Team team = new Team(8, 50);
+
+        assertEquals(4, team.calculateEffectiveWorkingHourInADay());
+    }
+
     private class Team {
-        public int getVelocity() {
-            return 16;
+        private final int workingHoursInADay;
+        private final int performance;
+
+        public Team(int workingHoursInADay, int performance) {
+            this.workingHoursInADay = workingHoursInADay;
+            this.performance = performance;
+        }
+
+
+        public int calculateEffectiveWorkingHourInADay() {
+            return (this.workingHoursInADay * performance)/100;
         }
     }
 }
